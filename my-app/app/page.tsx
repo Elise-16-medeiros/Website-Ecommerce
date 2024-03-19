c1567c6b57a713e66725b72665e49ac51b23ebef
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-03-19 11:35:24 -0300

    feat: first commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main>
+			<h1>Welcome.</h1>
+			<h2>Lorem ipsum dolor sit.</h2>
+			<h3>Lorem ipsum dolor sit.</h3>
+			<h4>Lorem ipsum dolor sit.</h4>
+			<h5>Lorem ipsum dolor sit.</h5>
+			<h6>Lorem ipsum dolor sit.</h6>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, vero?
+			</p>
+		</main>
+	);
+}
